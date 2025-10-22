822221b0fe6e75c38f456ccb080f5dad1d1fce4f
igbekele best <bwale974@gmail.com>
2025-10-22 17:36:40 +0100

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import create_string, get_single_string, get_all_strings, delete_string, filter_by_natural_language
+from . import views
 
 urlpatterns = [
-    path('strings/', create_string, name='create_string'),
-    path('strings/<str:string_value>/', get_single_string, name='get_single_string'),
-    path('strings/all/', get_all_strings, name='get_all_strings'),
-    path('strings/filter-by-natural-language/', filter_by_natural_language, name='filter_by_natural_language'),
-    path('strings/<str:string_value>/', delete_string, name='delete_string'),
+    path('strings', views.strings_view, name='strings'),  # GET, POST, DELETE
+    path('strings/<str:string_value>', views.string_detail_view, name='string-detail'),  # GET, DELETE
+    path('strings/filter-by-natural-language', views.strings_natural_filter_view, name='strings-natural-filter'),  # GET
 ]
