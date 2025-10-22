f0629c412ab86406c62f514817895f4952839b41
igbekele best <bwale974@gmail.com>
2025-10-22 18:17:05 +0100

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('strings', views.strings_view, name='strings'),  # GET, POST, DELETE
-    path('strings/<str:string_value>', views.string_detail_view, name='string-detail'),  # GET, DELETE
-    path('strings/filter-by-natural-language', views.strings_natural_filter_view, name='strings-natural-filter'),  # GET
+    path('strings/', views.strings_view, name='strings'),  # GET, POST, DELETE
+    path('strings/<str:string_value>/', views.string_detail_view, name='string-detail'),  # GET, DELETE
+    path('strings/filter-by-natural-language/', views.strings_natural_filter_view, name='strings-natural-filter'),  # GET
 ]
