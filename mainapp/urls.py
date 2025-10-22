90d515f5de11fe887862f0936e71b3211c2b5a8a
igbekele best <bwale974@gmail.com>
2025-10-22 18:18:02 +0100

    Remove trailing slash from natural language filter path

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('strings/', views.strings_view, name='strings'),  # GET, POST, DELETE
     path('strings/<str:string_value>/', views.string_detail_view, name='string-detail'),  # GET, DELETE
-    path('strings/filter-by-natural-language/', views.strings_natural_filter_view, name='strings-natural-filter'),  # GET
+    path('strings/filter-by-natural-language', views.strings_natural_filter_view, name='strings-natural-filter'),  # GET
 ]
